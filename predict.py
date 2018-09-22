2d819725729044f1ee4de670fb1f337d0618ec28
augustus <hikkikuma>
2018-09-22 13:34:09 +0800

    神经网络训练器

--- predict.py
+++ predict.py
@@ -1,4 +1,12 @@
-import os
+import pandas as pd
+import keras
+import numpy as np
 
 if __name__ == '__main__':
     print("Hello, World!")
+
+    baseinfo = pd.read_csv('dataset/1baseinfo.csv')
+
+    train = pd.read_csv('dataset/train.csv')
+
+    print(train)
